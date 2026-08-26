f3184f105287c862ea6129b21e4fb4367a958184
wilcox101-maker <wilcox101@gmail.com>
2026-08-26 14:23:05 -0400

    Include Mega Drive, Doom, and MSX in default apps

--- hiletgo-ws2-ns4168/env.py
+++ hiletgo-ws2-ns4168/env.py
@@ -1,4 +1,4 @@
 # HAND-32 / HiLetgo ESP32-S3-DevKitC N16R8
 IDF_TARGET = "esp32s3"
 FW_FORMAT = "none"
-DEFAULT_APPS = "launcher retro-core"
+DEFAULT_APPS = "launcher retro-core prboom-go gwenesis fmsx"
